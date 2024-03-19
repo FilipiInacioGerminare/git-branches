488c023400b13e2933c7ce9822f221708b84f375
Filipi Inácio Germinare <filipi.santos@germinare.org.br>
2024-03-19 09:18:30 -0300

    Arrumando cliente.java pois estava com código errado!

--- Cliente.java
+++ Cliente.java
@@ -1,10 +1,8 @@
 public class Cliente {
     private String nome;
-<<<<<<< HEAD
     private String RG;
-=======
     private String piscina;
->>>>>>> origin/classe-cliente
+
     private int agencia;
     private double saldo;
 }
